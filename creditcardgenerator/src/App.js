27795711a109c5ae47ffd53bafd3dfe7d50d99b6
pranilveer <veerpranil@gmail.com>
2023-10-02 23:28:47 +0530

    added card details page with validation

--- creditcardgenerator/src/App.js
+++ creditcardgenerator/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form'
+
 
 function App() {
   return (
-    <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <div className="App ">
+        <div className='left-div split'></div>
+        <div className='right-div split'>
+        <Form />
+        </div>
+        {/* <div className='card1 card '></div>
+        <div className='card2 card'></div> */}
     </div>
   );
 }
---
+++ creditcardgenerator/src/Form.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import "./form.css"
+
+function Form() {
+    const [cardHolderName, setCardHolderName] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [expMonth, setExpMonth] = useState('');
+    const [expYear, setExpYear] = useState('');
+    const [cvv, setCVV] = useState('');
+    const [error, setError] = useState(false);
+    const [submittedData, setSubmittedData] = useState(null);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (
+            cardHolderName.length === 0 ||
+            cardNumber.length === 0 ||
+            expMonth.length === 0 ||
+            expYear.length === 0 ||
+            cvv.length === 0
+        ) {
+            setError(true);
+            setSubmittedData(null);
+        } else {
+            setError(false);
+            setSubmittedData({
+                cardHolderName,
+                cardNumber,
+                expMonth,
+                expYear,
+                cvv,
+            })
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className='boxes'>
+                    <h5>CARDHOLDER NAME</h5>
+                    <input type='text' value={cardHolderName} placeholder='e.g. Jane Appleseed' onChange={e => setCardHolderName(e.target.value)} />
+                    {error && cardHolderName.length <= 0 ?
+                        <label>Cardholder name required</label> : ""}
+                </div>
+
+                <div className='boxes'>
+                    <h5>CARD NUMBER</h5>
+                    <input type='number' value={cardNumber} placeholder='e.g. 1234 5678 9123 0000' onChange={e => setCardNumber(e.target.value)} />
+                    {error &&  cardNumber.length <= 0 ?
+                        <label>Card number required</label> : ""}
+                </div>
+
+                <div className='info boxes'>
+                    <div className='date-div'>
+                        <h5>EXP. DATE (MM/YY)</h5>
+                        <div className='date-div-1'>
+                            <input type='number' value={expMonth} placeholder='MM' className='month-input box' onChange={e => setExpMonth(e.target.value)} />
+                            <input type='number' value={expYear} placeholder='YY' className='year-input box' onChange={e => setExpYear(e.target.value)} />
+                        </div>
+                        {error && (expMonth.length <= 0 || expYear.length <= 0) ?
+                            <label>Expiry date required</label> : ""}
+
+                    </div>
+
+                    <div className='cvv-div'>
+                        <h5>CVC</h5>
+
+                        <input type='number' value={cvv} placeholder='e.g. 123' className='cvv-input' onChange={e => setCVV(e.target.value)} />
+                        {error && cvv.length <= 0 ?
+                            <label>CVC must be numeric</label> : ""}
+                    </div>
+
+                </div>
+                <div className='boxes'>
+                    <button>Confirm</button>
+                </div>
+
+            </form>
+
+            {/* {submittedData && (
+                <div className="submitted-data">
+                    <p>{submittedData.cardHolderName}</p>
+                    <p>{submittedData.cardNumber}</p>
+                    <p>{submittedData.expMonth}/{submittedData.expYear}</p>
+                    <p>{submittedData.cvv}</p>
+                </div>
+            )} */}
+        </>
+    )
+}
+
+export default Form;
